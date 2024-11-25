a1eb6928c2745a5a44dd507dc64d21e795c28cdd
Derek Ratcliff <derek.ratcliff@gmail.com>
2024-11-25 16:47:00 -0500

    optimizations

--- d15/m2.py
+++ d15/m2.py
@@ -1,5 +1,3 @@
-from itertools import combinations
-import heapq
 from collections import deque
 OFFSETS = [(1, 0), (-1, 0), (0, -1), (0, 1)]
 OFFSETS_D = [(1, 0), (-1, 0), (0, -1), (0, 1),
@@ -28,7 +26,7 @@ G, W, H = to_grid(xs)
 LETTERS = {}
 for x in range(W):
     for y in range(H):
-        if G[(x, y)] not in ('#', '.', '~'):
+        if G[(x, y)] not in ('#', '.', '~', 'B', 'H', 'O', 'K'):
             letter = G[(x, y)]
             if letter not in LETTERS:
                 LETTERS[letter] = []
@@ -40,39 +38,25 @@ for x in range(W):
         START = (x, 0)
 
 
-# for each a given start_pt, find all
-# neighbors
-"""
-{
-  (x1,y1): c1,
-  (x2,y2): c2
-}
-
-"""
-
-
 def calc(G, start_pt, end_pt):
     Q = deque()
     visited = set()
-    hd = 0
     Q.append((start_pt, 0))
     neighbors = {}
-    start_letter = G[start_pt]
+    letter_keys = LETTERS.keys()
     while Q:
         (cur, dist) = Q.popleft()
-        # print(start_pt, cur, dist)
 
         if cur in visited:
             continue
         visited.add(cur)
 
         current_letter = G[cur]
-        if current_letter in LETTERS.keys() or cur == START:
+        if current_letter in letter_keys or cur == START:
             if dist != 0:
                 neighbors[cur] = dist
         for x, y in OFFSETS:
             nxt = (cur[0]+x, cur[1]+y)
-            # print("o", start_pt, cur, nxt)
             if nxt in G and G[nxt] not in ('#', '~'):
                 Q.append((nxt, dist+1))
 
@@ -96,80 +80,22 @@ for letter in LETTERS.keys():
             if e not in GRAPH[k] or v < GRAPH[k][e]:
                 GRAPH[k][e] = v
 
-NG = {}
-for c in combinations(list(LETTERS.keys()) + [G[START]], 2):
-    left = c[0]
-    right = c[1]
-    print("!!!", left, right)
-    min_pair = None
-    LEFT = [START] if left not in LETTERS else LETTERS[left]
-    RIGHT = [START] if right not in LETTERS else LETTERS[right]
-    for e in LEFT:
-        for f in RIGHT:
-            # print("left", left, e, "right", right, f)
-            if left not in NG:
-                NG[left] = {}
-            if right not in NG:
-                NG[right] = {}
-
-            left_right = GRAPH[e][f]
-            # print(e, f, left_right, NG)
-            if right not in NG[left] or left_right < NG[left][right][1]:
-                NG[left][right] = ((e, f), left_right)
-            if left not in NG[right] or left_right < NG[right][left][1]:
-                NG[right][left] = ((f, e), left_right)
-
-
-for k, v in GRAPH.items():
-    print("kv", k, v)
-
-NG2 = {}
-
-for k, v in NG.items():
-    for v2 in v.values():
-        a = v2[0]
-        left = a[0]
-        right = a[1]
-        dist = v2[1]
-        if left not in NG2:
-            NG2[left] = {}
-        NG2[left][right] = dist
-        if right not in NG2:
-            NG2[right] = {}
-        NG2[right][left] = dist
-
-for k, v in NG2.items():
-    print("kv2", k, v)
-
-# GRAPH = NG2
-
-"""
-what's the "best" result at a position?
-if we have been to a position before with the same letters found and a lower distance, we shouldn't continue
-{
- (x,y): ('A', 'B')
-}
-
-"""
-
 
 def calc2(G, GRAPH, start_pt):
-    Q = []
-    visited = {}
+    Q = deque()
     best_results = {}
-    best_to_find = {}
-    hd = 0
-    heapq.heappush(Q, (0, (start_pt, 0, [])))
+    Q.append((start_pt, 0, []))
     best_distance = None
+    closest = {}
+    to_find = len(LETTERS.keys())+1
     while Q:
-        priority, (cur, dist, found) = heapq.heappop(Q)
-        # print(cur, dist, found, G[cur])
-        if best_distance and dist > best_distance:
+        (cur, dist, found) = Q.pop()
+        if best_distance and dist >= best_distance:
             continue
-        found_ct = len(set(found))
-        if dist > 0 and G[cur] == '.' and found_ct < len(LETTERS.keys()) + 1:
+        found_ct = len(found)
+        if dist > 0 and G[cur] == '.' and found_ct < to_find:
             continue
-        found_letters = tuple(sorted(set(tuple(found + [G[cur]]))))
+        found_letters = tuple(sorted(found + [G[cur]]))
         if cur not in best_results:
             best_results[cur] = {}
         if found_letters not in best_results[cur] or dist < best_results[cur][found_letters]:
@@ -178,38 +104,26 @@ def calc2(G, GRAPH, start_pt):
             if len(found_letters) > 0:
                 continue
 
-        if found_ct > hd:
-            hd = found_ct
-            print("!!", hd, cur, dist, found, len(Q))
-        if found_ct == len(LETTERS.keys())+1 and G[cur] == '.':
-            print("!", cur, dist, found, len(Q))
+        if found_ct == to_find and G[cur] == '.':
+            print("!", cur, dist, found)
             if best_distance == None or dist < best_distance:
                 best_distance = dist
-        # visited.add(cur)
-        # print(GRAPH[cur])
-        options = {
+        options = {}
+        if cur in closest:
+            options = closest[cur]
+        else:
+            for (pos, dist1) in GRAPH[cur].items():
+                letter = G[pos]
+                if letter not in options or dist1 < options[letter][1]:
+                    options[letter] = (pos, dist1)
+            closest[cur] = options
 
-        }
-        for (pos, dist1) in GRAPH[cur].items():
-            letter = G[pos]
-            # if letter not in found:
-            heapq.heappush(Q, (-10000*found_ct+(dist+dist1),
-                           (pos, dist+dist1, found + [letter])))
+        for letter, (pos, dist1) in options.items():
+            if found_ct+1 != to_find and letter == '.':
+                continue
+            if letter not in found and (best_distance is None or dist+dist1 < best_distance):
+                Q.append((pos, dist+dist1, found + [letter]))
     return None
 
 
-# TO_START = {START: 0}
-# for k in GRAPH[START]:
-#     for k1 in GRAPH[START][k]:
-#         for pt in GRAPH[START][k][k1]:
-#             if pt not in TO_START or k1 < TO_START[pt]:
-#                 TO_START[pt] = k1
-
-print(LETTERS)
 calc2(G, GRAPH, START)
-
-# for k, v in GRAPH.items():
-#     n = set()
-#     for v1 in v:
-#         n.add(G[v1])
-#     print(k, n, len(n))
